59c136129323c274abc766c0b7d1aadc6efbda2e
woodtop <cwood89@mac.com>
2019-04-01 03:51:07 -0400

    initial file setup

---
+++ src/components/page/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Intro from "../intro"
+import Header from "../header"
+import Panel from "../panel"
+import Footer from "../footer"
+
+function Page() {
+  return (
+    <div>
+      <Intro />
+      <Header />
+      <Panel />
+      <Footer />
+    </div>
+  )
+}
+export default Page
---
+++ todo.md
@@ -0,0 +1,16 @@
+# todo
+i need to break down each element into respective components
+
+  intro
+  header
+  nav/tabs
+    about
+    contact
+    portfolio
+  footer/copyright
+
+use scss?
+add responsivenes
+link my work (host on server?) or link to github page?
+deploy
+set up domain name
